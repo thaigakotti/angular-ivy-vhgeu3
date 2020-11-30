84ec212d2fcb094aca596b2fa7dd3c561a8e198a
thaigakotti <thanigakotti@gmail.com>
2020-11-30 08:19:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-eqex1m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-eqex1m)
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+}
+const inputs = document.querySelectorAll(".input");
+
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
+});
